6f94ce6dd09f00c32e455178474309f97be4fdaa
Jesus Hernandez <jesushdzusa@gmail.com>
2022-08-25 01:52:13 -0500

    btn reset and age validation fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const messages=[
+const messages = [
     'embark on your journey with trust and a light heart for the world opens up before you.',
     'you are the master of your destiny and hold all the keys to success.',
     'you are the seer who stands between worlds and makes manifest the unmanifest.',
@@ -14,48 +14,97 @@ const messages=[
 
 ];
 
-let errorbox=document.getElementById('error');
-const getAmessage=()=>{
 
-    let name=document.getElementById('name');
-    let birth=document.getElementById('birth');
-    let message=document.getElementById('message');
-    let index=Math.floor(Math.random()*8)
-    let advice=messages[index];
-    let label=document.querySelector("label[for=Message]");
-    label.style.display='flex';
-    message.style.display='flex';
+
+const fieldsValidation = () => {
+    let name = document.getElementById('name').value;
+    let birth = document.getElementById('birth').value;
+    if (!name || !birth) {
+        alert("We don't have a message for you since there is no name or a birthday provided,dont let this opportunity go away, there is an advice waiting for you ");
+        return false;
+    }
+    return true;
+
+}
+
+const dateValidation = (fields) => {
+    let name = document.getElementById('name');
     //destructuring date of birthday for range age validation
-    let dateBirth= new Date(birth.value);
-    let birthYear=dateBirth.getFullYear();
-    let birthMonth=dateBirth.getMonth();
-    let birthDay=dateBirth.getDay(); 
+    let birth = document.getElementById('birth');
+    let birthYear = Number(birth.value.slice(0, 4));
+    let birthMonth = Number(birth.value.slice(5, 7));
+    let birthDay = Number(birth.value.slice(8, 10));
 
     //destructuring actual date for range age validation
-    let dateToday=new Date();
-    let todayYear=dateToday.getFullYear();
-    let todayMonth=dateToday.getMonth();
-    let todayDay=dateToday.getDay();
+    let dateToday = new Date();
+
+    let todayYear = dateToday.getFullYear();
+    let todayMonth = dateToday.getMonth() + 1;
+    let todayDay = dateToday.getDate();
+
+    
+    if (fields === true) {
+        if ((todayYear - birthYear) >= 18 && (todayMonth - birthMonth) >= 0 && (todayDay - birthDay) >= 0) {
+            return true;
+        }
+        else {
+            alert("You must be 18 years old to play");
+            birth.value = '';
+            name.value = '';
+
+            return false;
+
+        }
+    }
 
 
-    if(!name.value || !birth.value) message.innerHTML="We don't have a message for you since there is no name or a birthday provided,dont let this opportunity go away, there is an advice waiting for you ";
-    //condition for age's validation
-    else if((todayYear-birthYear)>=18 && (birthMonth-todayMonth)>=0 && (birthDay-todayDay)>=0){
-        message.innerHTML=`${name.value} ${advice}`;
-    }
-    else{
-        message.innerHTML=' ';
-        alert("You must be 18 years old to play");
-        name.value=' ';
-        birth.value=' ';
-    }
-    
-    
+
+
+}
+
+const reset = () => {
+    let btnreset=document.getElementById('reset');
+    btnreset.style.display='none';
+    let btnsubmit=document.getElementById('submit');
+    btnsubmit.style.display='flex';
+    let birth = document.getElementById('birth');
+    let name = document.getElementById('name');
+    let message = document.getElementById('message');
+    let label = document.querySelector("label[for=Message]");
+    birth.value = '';
+    name.value = '';
+    message.innerHTML='';
+    message.style.display='none';
+    label.style.display='none';
     
 }
 
-let button=document.getElementById('submit');
-button.addEventListener('click',()=>{
-    getAmessage();
+
+const getAmessage = (bol) => {
     
-});
+    if (bol === true) {
+        let btnsubmit=document.getElementById('submit');
+        let name = document.getElementById('name');
+        let message = document.getElementById('message');
+        let index = Math.floor(Math.random() * 8)
+        let advice = messages[index];
+        let label = document.querySelector("label[for=Message]");
+        label.style.display = 'flex';
+        message.style.display = 'flex';
+        message.innerHTML = `${name.value} ${advice}`;
+        let btnreset=document.getElementById('reset');
+        btnreset.style.display='flex';
+        btnsubmit.style.display='none';    
+    }   
+
+
+}
+
+
+document.addEventListener('click',e=>{
+    let target=e.target;
+
+    if(target.id==='submit') getAmessage(dateValidation(fieldsValidation()));
+    
+    else if(target.id==='reset') reset();
+})
